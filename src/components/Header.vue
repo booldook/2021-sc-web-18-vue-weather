7aa6642c56931c19d0feff443de4057a77c86c0b
booldook <booldook@gmail.com>
2021-06-10 13:59:48 +0900

    _variables.scss

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="header-wrapper">
+		<h1 class="logo">
+			<i class="fa fa-sun"></i>
+			<a href="">TODAY WEATHER</a>
+		</h1>
+		<ul class="navi-wrap">
+			<li class="navi">
+				<a href="">DAILY</a>
+			</li>
+			<li class="navi">
+				<a href="">5DAYS</a>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header-wrapper {
+	color: $colorLighter;
+	background-color: $colorDarker;
+	@include flex($SB, $CT);
+	.logo {
+		padding: $md
+	}
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount('#app')
